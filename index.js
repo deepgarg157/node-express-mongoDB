8e83511a90b9b3e9ce5aa1c7db0995c5eed133ec
unknown <deepgarg157@gmail.com>
2023-11-22 17:42:27 +0530

    node express mongoDB

--- index.js
+++ index.js
@@ -1,43 +1,69 @@
-const http = require('http');
-const fs = require('fs');
+// const lib = require('./lib.js')
 
-const index = fs.readFileSync('index.html', 'utf-8');
-const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
-const products = data.products;
+// console.log(lib.y)
 
-const server = http.createServer((req, res) => {
-     console.log(req.url, req.method);
 
-     if (req.url.startsWith('/product')) {
-          const id = req.url.split('/')[2]
-          const product = products.find(p => p.id === (+id))
-          console.log(product)
-          res.setHeader('Content-Type', 'text/html');
-          let modifiedIndex = index.replace('**title**', product.title)
-               .replace('**url**', product.thumbnail)
-               .replace('**price**', product.price)
-               .replace('**rating**', product.rating)
-          res.end(modifiedIndex);
-          return;
-     }
+// const http = require('http');
+// const fs = require('fs');
 
-     switch (req.url) {
-          case '/':
-               res.setHeader('Content-Type', 'text/html');
-               res.end(index);
-               break;
-          case '/api':
-               res.setHeader('Content-Type', 'application/json');
-               res.end(JSON.stringify(data));
-               break;
+// const index = fs.readFileSync('index.html', 'utf-8');
+// const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+// const products = data.products;
 
-          default:
-               res.writeHead(404);
-               res.end();
-     }
+// const server = http.createServer((req, res) => {
+//      console.log(req.url, req.method);
 
-     console.log('server started  ');
-     //   res.setHeader('Dummy', 'DummyValue');
-});
+//      if (req.url.startsWith('/product')) {
+//           const id = req.url.split('/')[2]
+//           const product = products.find(p => p.id === (+id))
+//           console.log(product)
+//           res.setHeader('Content-Type', 'text/html');
+//           let modifiedIndex = index.replace('**title**', product.title)
+//                .replace('**url**', product.thumbnail)
+//                .replace('**price**', product.price)
+//                .replace('**rating**', product.rating)
+//           res.end(modifiedIndex);
+//           return;
+//      }
 
-server.listen(8080);
+//      switch (req.url) {
+//           case '/':
+//                res.setHeader('Content-Type', 'text/html');
+//                res.end(index);
+//                break;
+//           case '/api':
+//                res.setHeader('Content-Type', 'application/json');
+//                res.end(JSON.stringify(data));
+//                break;
+
+//           default:
+//                res.writeHead(404);
+//                res.end();
+//      }
+
+//      console.log('server started  ');
+//      //   res.setHeader('Dummy', 'DummyValue');
+// });
+
+// server.listen(8080);
+
+// ============== Create the API through the HTTP create server ================
+const http = require('http')
+const data = require('./lib.js')
+
+const server = http.createServer((req, res)=>{
+      res.setHeader('Content-Text', 'application/json')
+      res.end(JSON.stringify(data))
+})
+
+server.listen(8080)
+
+// ================= Display the file form folder =================
+
+const fs = require('fs')
+const path = require('path');
+const dirPath = path.join(__dirname, 'files')
+
+for(i=0 ; i<5 ; i++){
+    fs.writeFileSync(dirPath + '/hello' + i + '.txt', 'this is a simple test file')
+}
---
+++ lib.js
@@ -0,0 +1,12 @@
+const x=10;
+const y=20
+
+exports.x = x
+exports.y = y
+
+const data = {
+    name : 'Deepanshu',
+    age : 27
+}
+
+module.exports = data;
