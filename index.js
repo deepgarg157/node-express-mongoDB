8989a41cfab8c0f0c4e805205d88c2f737f95494
unknown <deepgarg157@gmail.com>
2023-11-28 16:22:26 +0530

    Node Express MongoDB

--- index.js
+++ index.js
@@ -218,3 +218,18 @@ app.listen(5000)
 
 // ========== MongoDB ===============
 
+const {MongoClient} = require('mongodb')
+
+const url = 'mongodb://localhost:27017'
+const database = 'e-comm'
+const client = new MongoClient(url)
+
+async function getData (){
+    const result = await client.connect()
+    const db = result.db(database)
+    const collection = db.collection('products')
+    const response = await collection.find({}).toArray()
+    console.log(response)
+}
+
+getData()
