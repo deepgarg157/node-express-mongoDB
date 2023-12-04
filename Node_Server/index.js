6549dcc583487031452e7a4339269fabfb25e889
unknown <deepgarg157@gmail.com>
2023-12-04 12:41:47 +0530

    node express mongoDB

--- Node_Server/index.js
+++ Node_Server/index.js
@@ -5,14 +5,67 @@ const app = express()
 
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('this is a home page')
 })
 
-app.get('/user', (req, res)=>{
-    res.render('user')
+// app.get('/user', (req, res)=>{
+//     const userinfo =  {
+//         username:'deep',
+//         name:'Deepanshu',
+//         ahe:27,
+//         email:'deep@123gmail.com',
+//         password : 1234,
+//         isPremium : true
+//     }
+//     res.render('user', userinfo)
+// })
+
+// app.get('/users', (req, res)=>{
+//     const userinfo = {
+//         username:'ram',
+//         name:'ram',
+//         ahe:28,
+//         email:'ram@123gmail.com',
+//         password : 1234567,
+//         isPremium : false
+//     }
+//     res.render('user', userinfo)
+// })
+
+const USERS = [
+    {
+        username: 'deep',
+        name: 'Deepanshu',
+        ahe: 27,
+        email: 'deep@123gmail.com',
+        password: 1234,
+        isPremium: true
+    },
+    {
+        username: 'ram',
+        name: 'ram',
+        ahe: 28,
+        email: 'ram@123gmail.com',
+        password: 1234567,
+        isPremium: false
+    }
+]
+
+app.get('/not-found', (req, res) => {
+    res.render('not-found')
 })
 
-app.listen(7000, ()=>{
+app.get('/:username', (req, res) => {
+    console.log(req.params)
+    const { username } = req.params
+    const userDetails = USERS.find((user) => user.username === username)
+    if (userDetails)
+        res.render('user', userDetails)
+    else
+        res.redirect('not-found')
+})
+
+app.listen(7000, () => {
     console.log('server running')
 })
