797d7758238815fe4583980e7a840320d5154f79
unknown <deepgarg157@gmail.com>
2023-12-05 22:00:39 +0530

    node express mongodb

--- Node_Server2/index.js
+++ Node_Server2/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const ejs = require('ejs')
+const mongoose = require('mongoose')
 
 const app = express()
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended:false}))
+
+mongoose.connect('mongodb+srv://admin:admin@123@cluster0.zeg1dyy.mongodb.net/?retryWrites=true&w=majority')
+.then(()=>console.log('connected the data'))
+.catch((error)=> console.log(error))
 
 app.get('/',(req, res)=>{
     res.json({
