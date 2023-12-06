72cf77250c989ae994139930b49cc5517d866e4d
unknown <deepgarg157@gmail.com>
2023-12-06 11:12:44 +0530

    node express mongodb

--- Node_Server2/index.js
+++ Node_Server2/index.js
@@ -2,21 +2,22 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
 
 const app = express()
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended:false}))
 
-mongoose.connect('mongodb+srv://admin:admin@123@cluster0.zeg1dyy.mongodb.net/?retryWrites=true&w=majority')
-.then(()=>console.log('connected the data'))
-.catch((error)=> console.log(error))
-
 app.get('/',(req, res)=>{
+    console.log(process.env)
     res.json({
         message:'all good'
     })
 })
 
 app.listen(3000, ()=>{
-    console.log('server running on port 3000')
+// mongoose.connect('mongodb+srv://admin:admin123@cluster0.zeg1dyy.mongodb.net/?retryWrites=true&w=majority')
+// .then(()=>console.log('connected the data'))
+// .catch((error)=> console.log(error))
+//     console.log('server running on port 3000')
 })
