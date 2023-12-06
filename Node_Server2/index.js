9814840bc8a14358ffdb70c1c3c01f1788548722
unknown <deepgarg157@gmail.com>
2023-12-07 01:21:46 +0530

    Node express mongodb

--- Node_Server2/index.js
+++ Node_Server2/index.js
@@ -2,22 +2,102 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const mongoose = require('mongoose')
-const dotenv = require('dotenv').config()
+const dotenv = require('dotenv')
+dotenv.config()
 
 const app = express()
 app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/',(req, res)=>{
-    console.log(process.env)
+const Users = mongoose.model('user', {
+    firstName: String,
+    lastName: String,
+    phone: Number
+})
+
+app.get('/', (req, res) => {
     res.json({
-        message:'all good'
+        message: 'all good'
     })
 })
 
-app.listen(3000, ()=>{
-// mongoose.connect('mongodb+srv://admin:admin123@cluster0.zeg1dyy.mongodb.net/?retryWrites=true&w=majority')
-// .then(()=>console.log('connected the data'))
-// .catch((error)=> console.log(error))
-//     console.log('server running on port 3000')
+// CRUD
+// R - GET /users
+// C - Post /users
+// U - Patch /users/:id
+// D - Delete /users/:id
+
+app.get('/users', async (req, res) => {
+    try {
+        const user = await Users.find({})
+        res.json({
+            status: 'Success',
+            data: user
+        })
+    }
+    catch (error) {
+        res.json({
+            status: 'False',
+            message: error.message
+        })
+    }
+})
+
+app.post('/users', async (req, res) => {
+    console.log(req.body)
+    try {
+        const { firstName, lastName, phone } = req.body
+        await Users.create({
+            firstName, lastName, phone
+        })
+        res.json({
+            status: 'Success',
+            data: []
+        })
+    }
+    catch (error) {
+        res.json({
+            status: 'False',
+            message: error.message
+        })
+    }
+})
+
+app.patch('/users/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+        const { firstName, lastName, phone } = req.body
+        await Users.findByIdAndUpdate(id, {firstName, lastName, phone})
+        res.json({
+            status:'Success',
+            message:'user updated sucessfully'
+        })
+    } catch (error) {
+        res.json({
+            status: 'Fail',
+            message: error.message
+        })
+    }
+})
+
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+        await Users.findByIdAndDelete(id)
+        res.json({
+            status:'Success',
+            message:'user Deleted sucessfully'
+        })
+    } catch (error) {
+        res.json({
+            status: 'Fail',
+            message: error.message
+        })
+    }
+})
+
+app.listen(process.env.Port, () => {
+    mongoose.connect(process.env.MongoDB_URL)
+        .then(() => console.log('connected the data'))
+        .catch((error) => console.log(error))
 })
