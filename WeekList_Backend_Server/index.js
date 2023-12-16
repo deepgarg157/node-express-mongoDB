c51efa5faa18973a081a38a5aad9aa722132c820
unknown <deepgarg157@gmail.com>
2023-12-16 18:13:16 +0530

    node express mongodb

--- WeekList_Backend_Server/index.js
+++ WeekList_Backend_Server/index.js
@@ -21,6 +21,41 @@ const userInfo = mongoose.model('user', {
     isPremium: Boolean
 })
 
+// Middleware 
+const isLoggedIn = (req, res, next) => {
+    const logged = true
+    if (logged) {
+        const {jwttoken} = req.headers
+        jwt.verify(jwttoken, 'highlyCridentialSecurtyToken')
+        next()
+    }
+    else {
+        res.json({
+            status: 'try the user login again'
+        })
+    }
+}
+
+const isPremium = (req, res, next) => {
+    const premium = true
+    if (premium) {
+        next()
+    }
+    else {
+        res.json({
+            status: 'the user is not a premium account, pls take the premium account'
+        })
+    }
+}
+
+app.get('/dashboard', isLoggedIn, (req, res) => {
+    res.send('this is a dashboard page')
+})
+
+app.get('/isPremium', isLoggedIn, isPremium, (req, res) => {
+    res.send('This is a Premium Page')
+})
+
 app.get('/users', async (req, res) => {
     try {
         const user = await userInfo.find()
@@ -37,17 +72,54 @@ app.get('/users', async (req, res) => {
     }
 })
 
-app.post('/users', (req, res) => {
+app.post('/register', async (req, res) => {
     try {
         const { fullName, email, password, isPremium } = req.body
-        console.log(req.body)
+        const bcryptPassword = await bcrypt.hash(password, 10)
+        await userInfo.create({ fullName, email, password: bcryptPassword, isPremium })
         res.json({
-            status: 'Create the new user is successful',
+            status: 'Create the new register is successful',
+            password: bcryptPassword
         })
     }
     catch (error) {
         res.json({
             status: 'Fail to create the new user',
+            message: error.message
+        })
+    }
+})
+
+app.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const user = await userInfo.findOne({ email})
+        if (user) {
+            const passwordMatched = await bcrypt.compare(password, user.password)
+            if (passwordMatched) {
+                const jwtToken = jwt.sign(user.toJSON(), 'highlyCridentialSecurtyToken', { expiresIn: 15 })
+                res.json({
+                    status: 'success the user login',
+                    jwtToken
+                })
+            }
+            else {
+                res.json({
+                    status: 'Fail to logic'
+                })
+            }
+
+        }
+        else {
+            res.json({
+                status: 'Fail to user login try again'
+            })
+        }
+
+    }
+    catch (error) {
+        res.json({
+            status: 'Fail to user login',
             message: error.message
         })
     }
