9fa65d10983329f837e9c420dbb9d49247d1daf2
unknown <deepgarg157@gmail.com>
2023-12-16 15:34:46 +0530

    node express mondodb

--- Node_Server5/index.js
+++ Node_Server5/index.js
@@ -1,10 +1,44 @@
 const http = require('http')
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const {Schema, model} = require('mongoose')
+const dotenv = require('dotenv')
+const ejs = require('ejs')
+dotenv.config()
+
+const server = http.createServer((req, res)=>{
+    res.setHeader('Content-type', 'text-html')
+    res.end("server created")
+})
+
 const app = express()
+
+app.set('view engine', 'ejs')
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+const userInfo = new Schema({
+    fullName:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    isPremium:{
+        type:Boolean,
+        required:true
+    },
+})
+
+const Users = mongoose.model('user', userInfo)
 
 const Products = [
     {
@@ -37,6 +71,22 @@ const Products = [
     }
 ]
 
+app.get('/users', async (req, res) => {
+    try {
+        const user = await Users.find()
+        res.json({
+            status: 'success',
+            data: user
+        })
+    }
+    catch (error) {
+        res.json({
+            status: 'Fail',
+            message: error.message
+        })
+    }
+})
+
 app.get('/product', (req, res) => {
     const id = req.params.id
     // const product = Products.find((p) => p.id == id)
@@ -64,44 +114,54 @@ app.post('/product/:id', (req, res) => {
 
 })
 
-app.patch('/product/:id', (req, res)=>{
-    try{
+app.patch('/product/:id', (req, res) => {
+    try {
         res.json({
-            status:'Success',
-            message:'Update the product'
+            status: 'Success',
+            message: 'Update the product'
         })
     }
-    catch(error){
+    catch (error) {
         res.json({
-            status:'Fail',
-            message:error.message
+            status: 'Fail',
+            message: error.message
         })
     }
 })
 
 
-app.delete('/product/:id', (req, res)=>{
-    try{
+app.delete('/product/:id', (req, res) => {
+    try {
         const id = req.params.id
         const deleteProduct = Products.unshift()
         res.json({
-            status:'Success',
-            message:'delete the product'
+            status: 'Success',
+            message: 'delete the product'
         })
     }
-    catch(error){
+    catch (error) {
         res.json({
-            status:'Fail',
-            message:error.message
+            status: 'Fail',
+            message: error.message
         })
     }
 })
 
-app.listen(3000, () => {
-    console.log('server started')
+app.listen(process.env.port, () => {
+    mongoose.connect('mongodb+srv://admin:admin123@cluster0.zeg1dyy.mongodb.net/?retryWrites=true&w=majority')
+        .then(() => console.log('server started'))
+        .catch((error) => console.log(error.message))
 })
 
-// Express 
+// server.listen(3000)
+
+// Express
 // Rest API in express
 // CRUD in express
 // MVC - model view controller
+// EJS
+// MiddleWare router
+// Mongoose and mongoDB connect on the server
+// CRUD in the MongoDb and mongoose on the server
+// bcrypt - encrypt and decrypt
+// jwt - json web token
